de308b99b6c264d67a4c6b0b9030af9e0e15265f
Elaman Janyshbekov <elamanzanysbekov4@gmail.com>
2023-10-18 17:38:03 +0600

    Updates

---
+++ precache-manifest.8c4b6f7ef31f1443e47d40bf8208f8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15026b82fc7d3249cbec4612e314dff",
+    "url": "/hw-4/index.html"
+  },
+  {
+    "revision": "01e48a8fb01bd9830152",
+    "url": "/hw-4/static/css/main.fc088730.chunk.css"
+  },
+  {
+    "revision": "7d4969b8264884cbbe33",
+    "url": "/hw-4/static/js/2.2f266c80.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/hw-4/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "01e48a8fb01bd9830152",
+    "url": "/hw-4/static/js/main.961919a2.chunk.js"
+  },
+  {
+    "revision": "900e0b76bdadae2f42ab",
+    "url": "/hw-4/static/js/runtime~main.a54b86e4.js"
+  }
+]);
